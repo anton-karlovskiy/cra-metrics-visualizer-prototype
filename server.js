246061ed0dd0360d71d32c3123157abf4bfa6941
anton-karlovskiy <antonkarlovskiy@outlook.com>
2020-05-08 21:53:10 +0300

    chore: cleaned up

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ app.get(LIGHTHOUSE_ENDPOINT, async (req, res) => {
 app.get('/puppeteer', async (req, res) => {
   try {
     const browser = await puppeteer.launch({
-      args: ['--no-sandbox']
+      args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
     const page = await browser.newPage();
     await page.goto('https://developers.google.com/web/tools/puppeteer');
